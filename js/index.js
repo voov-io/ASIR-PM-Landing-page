4d3f26498f19757061a439695dd71f8889c06ae1
Jehaann Manzanares <jehaann07carlos@gmail.com>
2022-12-29 08:49:26 -0600

    ( update ) : optimizing images

--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
+let contenScroll = document.getElementById("contentscroll");
+let card = document.querySelector(".what_our__box ");
 document.addEventListener('click', (e) =>{
-    if (e.target.matches("#video-main")) {
+    if (e.target.matches("#submit")) {
+        //e.preventDefault()
+    }
+    if (e.target.matches("#scrollright")) {
+        
+        contenScroll.scrollLeft +=card.offsetWidth;
+        console.log(card.offsetWidth)
+    }
+    if (e.target.matches("#scrollleft")) {
+        
+        contenScroll.scrollLeft -=card.offsetWidth;
         
     }
+    if (e.target.matches("#video-main")) {
+        e.target.play();
+        if (e.target.currentTime > 0) {
+            e.target.style.height = "initial";
+            e.target.style.width = "54%"
+        }
+    }
+   
 })
